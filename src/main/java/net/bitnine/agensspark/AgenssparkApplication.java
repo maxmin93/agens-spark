2298902d9fa86e6daa2bc96efeaf166edf6c9049
Byeong-Guk Min <maxmin93@gmail.com>
2019-09-24 13:06:38 +0900

    add sparkSessionTest

--- src/main/java/net/bitnine/agensspark/AgenssparkApplication.java
+++ src/main/java/net/bitnine/agensspark/AgenssparkApplication.java
@@ -1,8 +1,12 @@
 package net.bitnine.agensspark;
 
+import com.google.common.collect.ImmutableMap;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import org.elasticsearch.spark.rdd.api.java.JavaEsSpark;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,21 +17,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class AgenssparkApplication implements ApplicationRunner {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(AgenssparkApplication.class);
+	static final Logger LOGGER = LoggerFactory.getLogger(AgenssparkApplication.class);
 
-	private String appName = "agensspark";
-	private String nodes = "27.117.163.21";
-	private String port = "15619";
-	private String sparkHome = "/Users/bgmin/Servers/spark";
-	private String resource = "elasticvertex";
-	private String extraCP = "/Users/bgmin/Servers/es-hadoop/dist/elasticsearch-hadoop-7.3.1.jar"
-				+ ",/Users/bgmin/Servers/es-hadoop/dist/elasticsearch-spark-20_2.11-7.3.1.jar";
+	String master = "local";
+	String appName = "agensspark";
+	String sparkHome = "/Users/bgmin/Servers/spark-2.4.3-bin-hadoop2.7";
+	String readResource = "elasticvertex";
+	String writeResource = "sparktemp";
+//	String extraCP = "/Users/bgmin/Servers/es-hadoop/dist/elasticsearch-hadoop-7.3.1.jar"
+//				+ ",/Users/bgmin/Servers/es-hadoop/dist/elasticsearch-spark-20_2.11-7.3.1.jar";
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgenssparkApplication.class, args);
@@ -37,36 +42,70 @@ public class AgenssparkApplication implements ApplicationRunner {
 	public void run(ApplicationArguments applicationArguments) throws Exception {
 
 		LOGGER.info("SpringBootSpark To Elasticsearch Application: {}, {}, {}, {}"
-				, appName, nodes, port, sparkHome);
+				, master, appName, sparkHome, readResource);
 
-		String master = "local[*]";
-		SparkConf conf = new SparkConf()
+		// When run by java -jar, Fail
+		javaEsSparkTest();
+
+		// When run by java -jar, Fail also
+		// sparkSessionTest();
+	}
+
+	private SparkConf sparkConf(){
+		return new SparkConf()
 				.setAppName(appName)
 				.setSparkHome(sparkHome)
 				.setMaster(master)
-                .set("spark.executor.memory", "2g")
+				.set("spark.executor.memory", "2g")
 				.set("spark.driver.memory", "2g")
 				.set("spark.eventLog.enabled","false")
-				.set("spark.driver.extraClassPath", extraCP)
-				// es 접속정보
-				.set("es.nodes.wan.only", "true")
-				.set("es.nodes", nodes)
-				.set("es.port", port)
-				.set("es.mapping.id", "id")
-				.set("es.write.operation", "upsert")
-				.set("es.index.read.missing.as.empty", "true");
+				;
+	}
 
-		JavaSparkContext jsc = new JavaSparkContext(conf);
+	private Map<String,String> esConf(){
+		Map<String,String> conf = new HashMap<>();
+		conf.put("es.nodes", "27.117.163.21");
+		conf.put("es.port", "15619");
+		conf.put("es.nodes.wan.only", "true");
+		conf.put("es.mapping.id", "id");
+		conf.put("es.write.operation", "upsert");
+		conf.put("es.index.auto.create", "true");
+		conf.put("es.scroll.size", "10000");
+		conf.put("es.mapping.exclude", "removed,*.present");
+		return ImmutableMap.copyOf(conf);
+	}
+
+	private void sparkSessionTest(){
+		final SparkConf conf = sparkConf();
+		final SparkSession sql = SparkSession.builder().config(conf).getOrCreate();
+		sql.sparkContext().setLogLevel("ERROR");
+
+		String datasource = "modern";
+		Dataset<Row> readDF = sql.read().format("es").options(esConf())
+				.option("es.query", "?q=datasource:"+datasource)
+				.load(readResource);
+
+		readDF.printSchema();
+		readDF.show();
 
 		// for DEBUG
 		System.out.println("\n\n==================================\n");
-		System.out.println(Arrays.stream(jsc.getConf().getAll()).map(t->{
-			return t._1()+"="+t._2();
-		}).collect(Collectors.joining(", ")));
+		System.out.println("\n**count = "+readDF.count());
+	}
 
-		JavaPairRDD<String, Map<String, Object>> esRDD =
-				JavaEsSpark.esRDD(jsc, resource, "?q=datasource:sample");
+	private void javaEsSparkTest(){
+		final SparkConf conf = sparkConf();
+		final JavaSparkContext jsc = new JavaSparkContext(conf);
+
+		String datasource = "modern";
+		Map<String,String> cfgEsHadoop = new HashMap<>(esConf());
+		cfgEsHadoop.put("es.resource", readResource);
+		cfgEsHadoop.put("es.query", "?q=datasource:"+datasource);
+
+		JavaPairRDD<String, Map<String, Object>> esRDD = JavaEsSpark.esRDD(jsc, cfgEsHadoop);
+
+		// for DEBUG
+		System.out.println("\n\n==================================\n");
 		System.out.println("\n**count = "+esRDD.count());
-
 	}
 }
